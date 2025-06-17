7721fe88a18686347d8de4ea34f7310a35000515
Ben <bcbrad@gmail.com>
2025-06-17 06:40:41 -0600

    auth middle ware in progress

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/bencbradshaw/framework"
 )
 
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("AUTH CHECK: [%s %s]\n", r.Method, r.URL.Path)
+		_, err := r.Cookie("framework")
+		if err != nil {
+			framework.RenderWithHtmlResponse(w, "error.html", map[string]any{
+				"title": "Unauthorized",
+				"code":  401,
+			})
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	mux := framework.Run(nil)
+
+	mux := framework.Run(framework.InitParams{
+		AuthGuard:                  AuthMiddleware,
+		IsDevMode:                  true,
+		AutoRegisterTemplateRoutes: true,
+	})
 
 	mux.Handle("/shop", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		framework.RenderWithHtmlResponse(w, "shop.custom.html", map[string]any{
@@ -16,6 +37,7 @@ func main() {
 			"Items": []string{"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"},
 		})
 	}))
+
 	print("Server started on http://localhost:2026\n")
 	http.ListenAndServe(":2026", mux)
 }
